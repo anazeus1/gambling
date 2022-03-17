3954a4095b229c1ef50d75117b6d2e0c89ca955d
anazeus1 <anasgh31@gmail.com>
2022-03-17 20:34:02 +0100

    version 1.2 adding automatic coordinates

--- martilgale/martilgale_roulette.py
+++ martilgale/martilgale_roulette.py
@@ -8,7 +8,7 @@ import time
 
 # this method moves the mouse cursor to the coordinates and clicks num-clicks times
 def click_coordinates(x, y, num_clicks=1):
-    pyautogui.click(x, y, num_clicks, interval=0.25, button="left")
+    pyautogui.click(x, y, num_clicks, interval=0, button="left")
 
 
 def get_postion():
@@ -37,29 +37,30 @@ def get_postion():
 
 def get_number_from_clipboard():
     pyautogui.hotkey("ctrl", "c")
-    text = pyp.paste()
-    text1 = text.replace(" ", "")
+    text1 = pyp.paste()
+    #text1 = text.replace(" ", "")
     number = 50
-    number_place = text1.find(">")
-    if(text1[number_place+3:number_place+4].isdigit()):
-        number = int(text1[number_place+3])
-    elif(text1[number_place+3].isdigit()):
-        number = int(text1[number_place+3])
+    #number_place = text1.find(">")
+    if(text1[55:57].isdigit()):
+        number = int(text1[55:57])
+    elif(text1[55].isdigit()):
+        number = int(text1[55])
     return number
 
 
-time.sleep(3)
+f = open("C:\\Users\\main laptop\\programation\\algorthims coursera\\gambling\\martilgale\\result.txt", "w")
+
 
 # gets coordinates from the chosen roulette site
 x_black, y_black, x_red, y_red, x_html, y_html, x_button, y_button = get_postion()
 
 # bets on red to begin the sequenceand goes to html to get changing number
-print(get_number_from_clipboard())
-click_coordinates(x_red, y_red, 3)
-click_coordinates(x_button, y_button)
+click_coordinates(x_red, y_red)
+#click_coordinates(x_button, y_button)
 click_coordinates(x_html, y_html)
 winning_number = []
 winning_number.append(get_number_from_clipboard())
+f.write((str(winning_number) + " red " + str(1) + "\n"))
 
 martilgale = ["red", "red", "black", "red", "black",
               "black"]  # martilgale sequence sequence
@@ -75,24 +76,31 @@ while(True):
     if winning_number[-1] != number and number < 37:  # if the result changes we bet
         i = i % 6
         winning_number.append(number)
+        f.write(str(number)+" \n")
 
-        # check if number is red or black or 0
+        # check if number is red <or black or 0
         if (number in red_numbers):
             color = "red"
+
         elif(number in black_numbers):
             color = "black"
+
         else:
             color = 0
 
-        time.sleep(4)
+        time.sleep(2.5)
         # if color is the same color we betted on we only bet 1 unit on the next color in the sequence
         if color == martilgale[i % 6]:
             bet = 1
             if martilgale[(i+1) % 6] == "red":
                 click_coordinates(x_red, y_red)  # bet red
+                f.write((" red " + str(1) + "\n"))
+
             else:
                 click_coordinates(x_black, y_black)  # bet black
-            click_coordinates(x_button, y_button)  # click rotate
+                f.write((" black " + str(1) + "\n"))
+
+            # click_coordinates(x_button, y_button)  # click rotate
             # return to html and wait for number
             click_coordinates(x_html, y_html)
 
@@ -101,9 +109,11 @@ while(True):
             bet = bet*2
             if martilgale[(i+1) % 6] == "red":
                 click_coordinates(x_red, y_red, bet)
+                f.write(("red " + str(bet) + "\n"))
             else:
                 click_coordinates(x_black, y_black, bet)
-
-            click_coordinates(x_button, y_button)
+                f.write((" black " + str(1) + "\n"))
+            #click_coordinates(x_button, y_button)
             click_coordinates(x_html, y_html)
+
         i = i+1  # go to next color in sequence
